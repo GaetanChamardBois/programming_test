2e27688b985c3d591424aab6d4fe171723b0a118
GaetanChamardBois <86894731+GaetanChamardBois@users.noreply.github.com>
2021-07-04 15:57:00 +0200

    Add files via upload

--- exercise.py
+++ exercise.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Logic
 
@@ -9,8 +10,9 @@ def check_uniqueness(lst):
     Check if a list contains only unique values.
     Returns True only if all values in the list are unique, False otherwise
     """
-    pass
-
+    
+    return len(set(lst)) == len(lst)
+    
 
 def smallest_difference(array):
     """
@@ -19,7 +21,10 @@ def smallest_difference(array):
     Please note that the array can be large and that the more
     computationally efficient the better
     """
-    pass
+    # absolute difference is small when elements are small
+    # hence, we sort the array and look at the abs diff of its elements
+    
+    return np.min(np.abs(np.diff(np.sort(array))))
 
 
 # Finance and DataFrame manipulation
@@ -31,9 +36,20 @@ def macd(prices, window_short=13, window_long=26):
     returns it's MACD (Moving Average Convergence Difference) as
     a DataFrame with same shape
     Assume simple moving average rather than exponential moving average
-    The expected output is in the output.csv file
+    The expected output is in the output.csv file   
     """
-    pass
+    macd_df = prices.copy() # create a new df
+    
+    prices_col = np.array(range(len(prices.columns)))
+    
+    # We check whether the first column is the date vector
+    if type(prices.iloc[0,0]) == str:
+        prices_col = np.delete(prices_col,0) # we remove the date from the stock list
+        
+    macd_df.iloc[:,prices_col] =  (prices.iloc[:,prices_col].rolling(window_short).mean() - 
+                    prices.iloc[:,prices_col].rolling(window_long).mean()).round(4)
+    
+    return macd_df
 
 
 def sortino_ratio(prices):
@@ -43,7 +59,18 @@ def sortino_ratio(prices):
     Assume risk-free rate = 0
     On the given test set, it should yield 0.05457
     """
-    pass
+        
+    prices_col = np.array(range(len(prices.columns)))
+    
+    # We check whether the first column is the date vector
+    if type(prices.iloc[0,0]) == str:
+        prices_col = np.delete(prices_col,0) # we remove the date from the stock list
+    
+    ret = prices.iloc[:,prices_col].pct_change()    
+    
+    std_down = ret[ret<0].std()
+    
+    return ret.mean() / std_down
 
 
 def expected_shortfall(prices, level=0.95):
@@ -52,8 +79,19 @@ def expected_shortfall(prices, level=0.95):
     returns the expected shortfall at a given level
     On the given test set, it should yield -0.03468
     """
-    pass
-
+    prices_col = np.array(range(len(prices.columns)))
+    
+    # We check whether the first column is the date vector
+    if type(prices.iloc[0,0]) == str:
+        prices_col = np.delete(prices_col,0) # we remove the date from the stock list
+    
+    # get returns
+    ret = prices.iloc[:,prices_col].pct_change() 
+    
+    # compute VaR
+    VaR = - ret.quantile(1-level)
+    #return the ES
+    return  ret[ret < - VaR].mean()
 
 # Plot
 
@@ -63,4 +101,17 @@ def visualize(prices, path):
     Code a function that takes a DataFrame named prices and
     saves the plot to the given path
     """
-    pass
+    prices_col = np.array(range(len(prices.columns)))
+    dates = prices.index # Default
+    # We check whether the first column is the date vector
+    if type(prices.iloc[0,0]) == str:
+        prices_col = np.delete(prices_col,0) # we remove the date from the stock list
+        dates = pd.to_datetime(prices.iloc[:,0])
+        
+    plt.figure(figsize = (10,6))
+    plt.plot(dates,prices.iloc[:,prices_col],label=prices.columns[prices_col])
+    plt.title('prices')
+    plt.legend()
+    plt.xlabel('date')
+    plt.ylabel('price')
+    plt.savefig(path)
